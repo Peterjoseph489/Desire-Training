cd0e0ed1d59d91b332ff50014e3e6031d5899c60
Joseph <josephpeter489@gmail.com>
2023-07-02 20:32:59 +0100

    commit

---
+++ desire.js
@@ -0,0 +1,69 @@
+// WELCOME TO JAVASCRIPT
+
+// JAVASCRIPT DATATYPES
+// 1. String: eg. "I want a car", "It can be Anything", 'Single Qoute
+// 2. Number: eg. 45, 67, 3, 8.9, -3.
+// 3. Boolean: eg. true, false
+// 4. Undefined: eg. Undefined
+// 5. Null: eg. null
+// 6. Symbol: eg. /,.[{%^&*
+
+
+// Object: {key: value}, {color: "blue"}
+// Array: eg. ["desire"]
+
+let call = "I want a car"
+console.log(call);
+
+let bb = 54
+console.log(bb);
+
+let aa = "54"
+console.log(aa);
+
+let cc = false
+console.log(cc);
+console.log(typeof(cc));
+
+// Undefined
+// console.log(dd);
+// console.log(typeof(dd));
+
+// Variables: storing a data into a container
+// Var
+// let
+// const
+
+
+let a = 5;
+const b = 2;
+const c = a * b;
+let d =  6;
+const e = d = a;
+console.log(e);
+
+// =
+// ==
+// ===
+
+let g = 53;
+let h = 7;
+g -= h
+console.log(g);
+
+
+let gg = 54;
+let hg = "54";
+
+// Write the following Program
+
+// 1. Declare the following using different varaible keywords (78, car, mother, 81, 23, 12)
+// 2. Add the variables that contain numbers and assign the result to "solution".
+// 3. console.log each of the following variables.
+
+
+
+
+
+
+
