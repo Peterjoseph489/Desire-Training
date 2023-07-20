854483e2fcaf368f60fc859302912aa60de20152
Joseph <josephpeter489@gmail.com>
2023-07-20 21:53:53 +0100

    array methods

--- desire.js
+++ desire.js
@@ -12,58 +12,153 @@
 // Object: {key: value}, {color: "blue"}
 // Array: eg. ["desire"]
 
-let call = "I want a car"
-console.log(call);
+// // let call = "I want a car"
+// // console.log(call);
 
-let bb = 54
-console.log(bb);
+// // let bb = 54
+// // console.log(bb);
 
-let aa = "54"
-console.log(aa);
+// // let aa = "54"
+// // console.log(aa);
 
-let cc = false
-console.log(cc);
-console.log(typeof(cc));
+// // let cc = false
+// // console.log(cc);
+// // console.log(typeof(cc));
 
-// Undefined
-// console.log(dd);
-// console.log(typeof(dd));
+// // Undefined
+// // console.log(dd);
+// // console.log(typeof(dd));
 
-// Variables: storing a data into a container
-// Var
-// let
-// const
-
-
-let a = 5;
-const b = 2;
-const c = a * b;
-let d =  6;
-const e = d = a;
-console.log(e);
-
-// =
-// ==
-// ===
-
-let g = 53;
-let h = 7;
-g -= h
-console.log(g);
+// // Variables: storing a data into a container
+// // Var
+// // let
+// // const
 
 
-let gg = 54;
-let hg = "54";
+// // let a = 5;
+// // const b = 2;
+// // const c = a * b;
+// // let d =  6;
+// // const e = d = a;
+// // console.log(e);
 
-// Write the following Program
+// // =
+// // ==
+// // ===
 
-// 1. Declare the following using different varaible keywords (78, car, mother, 81, 23, 12)
-// 2. Add the variables that contain numbers and assign the result to "solution".
-// 3. console.log each of the following variables.
+// // let g = 53;
+// // let h = 7;
+// // g -= h
+// // console.log(g);
 
 
+// // let gg = 54;
+// // let hg = "54";
+
+// // Write the following Program
+
+// // 1. Declare the following using different variable keywords (78, car, mother, 81, 23, 12)
+// // 2. Add the variables that contain numbers and assign the result to "solution".
+// // 3. console.log each of the following variables.
 
 
 
+// const ada = 78; 
+// let obi = "car";
+// var president = "mother";
+// let desire = 81;
+// const didi = 23;
+// var valentine = 12;
 
+// let objects = {
+//     ada: 78000000002,
+//     obi: "car",
+//     president: "mother",
+//     desire: 81,
+//     didi: 23,
+//     valentine: 12
+// }
+// console.log(objects.ada)
+
+// const solution = ada + desire + didi + valentine;
+// console.log(ada);
+// console.log(obi);
+// console.log(president)
+// console.log(desire)
+// console.log(didi)
+// console.log(valentine)
+// console.log(solution);
+// console.log("The solution of the sum of the number variables is: " +solution)
+// console.log(`The solution of the sum of the number variables is: ${solution}`)
+
+// // Pereference Order for Qoutes:
+// //  ``
+// //  ""
+// //  ''
+
+// console.log('I am desire Ude')
+// console.log("I'm desire Ude ${solution}")
+// console.log(`I'm desire Ude ${solution}`)
+
+
+// // ARRAY
+// // An array is a special variable that can be used to store information.
+// // 78, car, mother, 81, 23, 12
+// const salvationArray = [78, "car", "mother", 81, true, 12]
+// console.log(salvationArray)
+
+// // OBJECT
+// // An object is a special variable that can be used to store information.
+// // 78, car, mother, 81, 23, 12
+// // Key and value
+// // colour: red;
+// // club: chelsea;
+// // venue: lagos;
+// const salvationObject ={
+//     colour: 'red',
+//     club: 'chelsea',
+//     venue: 'lagos'
+// }
+// console.log(salvationObject)
+
+// let object = {
+//     ada: 78,
+//     obi: "car",
+//     president: "mother",
+//     desire: 81,
+//     didi: 23,
+//     valentine: 12
+// }
+
+// console.log(salvationObject.colour)
+
+// salvationObject.colour = 'green'
+
+// console.log(salvationObject.colour)
+
+// console.log(salvationObject);
+
+
+// ARRAY METHODS
+const desire = [78, "car", "mother", 81, true, 12]
+// ARRAY PROPERTIES
+// console.log(salvationsArray.length)
+// console.log(salvationsArray[5]);
+
+// console.log(salvationsArray[salvationsArray.length - 1])
+// console.log(salvationsArray.join('-'))
+// console.log(salvationsArray.toString());
+
+let abc = desire.pop();
+console.log(abc)
+
+console.log(desire)
+
+const ude = desire.push(abc)
+console.log(desire)
+
+
+
+// toString(), length, concat(), typeof, pop(), push(), shift(), unshift(), join().
+// Assignment
 
